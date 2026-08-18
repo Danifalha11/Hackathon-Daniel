4bfa0fa41bc1389291096b17be4519dacb0e24b3
Danifalha11 <mariadelourdessilveira142@gmail.com>
2026-08-18 13:09:26 -0300

    login

--- login.py
+++ login.py
@@ -1,22 +1,48 @@
 import mysql.connector
 import os
+import time
 from SQLxPython import *
 from cadastros import *
 
 def fazerLogin():
-    os.system('cls' if os.name == 'nt' else 'clear')
-    print("------LOGIN------")
-    print("1 - Já tenho uma conta")
-    print("2 - Criar uma conta")
-    escolha = input("")
+    while True:
+        os.system('cls' if os.name == 'nt' else 'clear')
+        print("--------LOGIN--------")
+        print("1 - Já tenho uma conta")
+        print("2 - Criar uma conta")
+        escolha = input("")
 
-    if escolha == "1":
-        ...
-    elif escolha == "2":
-        cadastrarUser()
-    else:
-        print("Opção inválida")
-        time.sleep(2)
-        return
+        if escolha == "1":
+            user = input("\nDigite seu usuário: ")
+            senha = input("\nDigite sua senha: ")
+
+            conn = conectar()
+            cursor = conn.cursor(dictionary=True)
+
+            try:
+                sql = "SELECT id_user, nome, senha, pontuacao FROM usuarios WHERE nome = %s AND senha = %s"
+                cursor.execute(sql, (user,senha))
+                usuario = cursor.fetchone()
+
+                if usuario:
+                    print(f"Login realizado, bem vindo {usuario['nome']}")
+                    time.sleep(3)
+                    return usuario
+                else:
+                    print("Usuário ou senha incorreto(s)")
+                    time.sleep(3)
+            except Error as e:
+                print(f"Erro no banco: {e}")
+                time.sleep(5)
+                return
+            finally:
+                cursor.close()
+                conn.close()
+
+        elif escolha == "2":
+            cadastrarUser()
+        else:
+            print("Opção inválida")
+            time.sleep(2)
 
 fazerLogin()
