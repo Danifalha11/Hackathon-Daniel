a25dd99469c9882243473957005816a71c4e6cf9
Danifalha11 <mariadelourdessilveira142@gmail.com>
2026-08-19 08:29:26 -0300

    QoL levels

--- levels.py
+++ levels.py
@@ -1,14 +1,24 @@
 import time
+import os
+from SQLxPython import *
+pontos = 0
 
 def level1():
+    conn = conectar()
+    cursor = conn.cursor(dictionary=True)
+
+    os.system('cls' if os.name == 'nt' else 'clear')
     print("level 1:")
+    time.sleep(1.5)
     chances = 3 
     print(f"Chances restantes: {chances}")
+    time.sleep(3)
     while True:
        
 
     
         while True:
+            os.system('cls' if os.name == 'nt' else 'clear')
             print("Pergunta 1 - Quantas lixeiras recicláveis existem?")
             print("a) 4 cores principais (Papel, Plástico, Vidro, Metal)")
             print("b) 10 cores padrão de coleta seletiva (incluindo orgânico, hospitalar, perigoso, etc.)")
@@ -17,19 +27,24 @@ def level1():
             if resposta1_1 == "b":
                 print("resposta certa\n")
                 print(f"Chances restantes: {chances}")
+                pontos += 1
+                time.sleep(2)
                 break
             elif resposta1_1 in ["a", "c"]:
                 print("resposta errada\n")
                 chances -= 1
                 print(f"Chances restantes: {chances}")
+                time.sleep(2)
                 break
             else:
                 print("escolha uma opção válida\n")
+                time.sleep(1)
                 continue
             
 
         
         while True:
+            os.system('cls' if os.name == 'nt' else 'clear')
             print("Pergunta 2 - Qual energia vem do Sol?")
             print("a) Energia Eólica")
             print("b) Energia Solar")
@@ -38,18 +53,23 @@ def level1():
             if resposta1_2 == "b":
                 print("resposta certa\n")
                 print(f"Chances restantes: {chances}")
+                pontos += 1
+                time.sleep(2)
                 break
             elif resposta1_2 in ["a", "c"]:
                 print("resposta errada\n")
                 chances -= 1
                 print(f"Chances restantes: {chances}")
+                time.sleep(2)
                 break
             else:
                 print("escolha uma opção válida\n")
+                time.sleep(1)
                 continue
 
         
         while True:
+            os.system('cls' if os.name == 'nt' else 'clear')
             print("Pergunta 3 - Qual energia vem do vento?")
             print("a) Energia Eólica")
             print("b) Energia Hídrica")
@@ -58,21 +78,27 @@ def level1():
             if resposta1_3 == "a":
                 print("resposta certa\n")
                 print(f"Chances restantes: {chances}")
+                pontos += 1
+                time.sleep(2)
                 break
             elif resposta1_3 in ["b", "c"]:
                 print("resposta errada\n")
                 chances -= 1
                 print(f"Chances restantes: {chances}")
+                time.sleep(2)
                 if chances == 0:
-                    print("Você perdeu todas as chances! Game Over.")   
+                    print("Você perdeu todas as chances! Game Over.")
+                    time.sleep(5)   
                     return
                 break
             else:
                 print("escolha uma opção válida\n")
+                time.sleep(1)
                 continue
 
         
         while True:
+            os.system('cls' if os.name == 'nt' else 'clear')
             print("Pergunta 4 - O que podemos plantar para ajudar o ambiente?")
             print("a) Plantas exóticas invasoras")
             print("b) Árvores nativas e plantas melíferas (que atraem abelhas)")
@@ -81,21 +107,27 @@ def level1():
             if resposta1_4 == "b":
                 print("resposta certa\n")
                 print(f"Chances restantes: {chances}")
+                pontos += 1
+                time.sleep(2)
                 break
             elif resposta1_4 in ["a", "c"]:
                 print("resposta errada\n")
                 chances -= 1
                 print(f"Chances restantes: {chances}")
+                time.sleep(2)
                 if chances == 0:
-                    print("Você perdeu todas as chances! Game Over.")   
+                    print("Você perdeu todas as chances! Game Over.")
+                    time.sleep(5)   
                     return
                 break
             else:
                 print("escolha uma opção válida\n")
+                time.sleep(1)
                 continue
 
         
         while True:
+            os.system('cls' if os.name == 'nt' else 'clear')
             print("Pergunta 5 - Qual cor da lixeira representa papel?")
             print("a) Amarelo")
             print("b) Verde")
@@ -104,19 +136,27 @@ def level1():
             if resposta1_5 == "c":
                 print("resposta certa\n")
                 print(f"Chances restantes: {chances}")
+                pontos += 1
+                time.sleep(2)
                 break
             elif resposta1_5 in ["a", "b"]:
                 print("resposta errada\n")
                 chances -= 1
                 print(f"Chances restantes: {chances}")
+                time.sleep(2)
                 if chances == 0:
-                    print("Você perdeu todas as chances! Game Over.")   
+                    print("Você perdeu todas as chances! Game Over.")
+                    time.sleep(5)
                     return
                 break
             else:
                 print("escolha uma opção válida\n")
+                time.sleep(1)
                 continue
 
+        sql = "UPDATE usuarios SET pontuacao = pontuacao + %s WHERE id_user = %s"
+        cursor.execute(sql, (pontos, usuario['id_user']))
+        conn.commit()
         print(f"Fim do Level 1! Chances restantes: {chances}")
         time.sleep(5)
         break
